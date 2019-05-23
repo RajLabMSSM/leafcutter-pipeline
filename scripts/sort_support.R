502169e9fbdf523e14968a2102aaa9bf4d9f0a52
Jack Humphrey <jack.humphrey@mssm.edu>
2019-05-23 16:58:56 -0400

    made sort_support.R more robust with testing

--- scripts/sort_support.R
+++ scripts/sort_support.R
@@ -28,21 +28,42 @@ outFolder <- opt$outFolder
 
 outFile <- paste0(outFolder, "/", dataCode, "_ds_support.tsv")
 
+message("Creating leafcutter support file from provided metadata")
+message("-------------------------------------------------------")
+
+if( ! file.exists(outFile ) ){ stop("metadata does not exist!" ) }
+
 
 df <- read.table(metadata, header=TRUE, sep = "\t", stringsAsFactors = FALSE)
 
-names(df)[2] <- "condition"
+if( names(df)[2] != "condition" ){
+	stop("the second column must be \"condition\" ")
+}
 
 # remove any samples that have a condition not in ref or alt
+
+if( ! all( c(refCondition, altCondition) %in% df$condition) ){
+	stop("neither condition is in metadata")
+}
+
 
 df <- df[ df$condition %in% c(refCondition, altCondition),]
 
 # leafcutter assumes first string in second column is the reference condition, so reorder table if necessary
 
+
 df$condition <- factor(df$condition, levels = c(refCondition, altCondition))
 
 df <- df[ order(df$condition),]
 
+if(nrow(df) == 0){
+	stop("table is empty")
+}
+
+print(df)
+
 # write out
 
+message("creating support file")
+message(paste0("saving as ", outFile) )
 write.table(df, file = outFile, col.names=FALSE, sep = "\t", quote = FALSE, row.names = FALSE)
